5ec80ccfed7b073030e30e9e6cd0c59d8083bd39
Jamie Kennea <jak574@gmail.com>
2025-03-21 17:50:40 -0400

    Add config

---
+++ across/tools/core/config.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    """Base configuration for the application."""
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class Config(BaseConfig):
+    """Configuration for the application.
+    The Config class extends BaseConfig and provides configuration settings for the application,
+    including environment, hosting, logging, and request-related parameters.
+
+    Attributes
+    ----------
+    SPACETRACK_USER : str
+        Space-Track.org username
+    SPACETRACK_PWD  : str
+        Space-Track.org password
+    """
+
+    SPACETRACK_USER: Optional[str] = None
+    SPACETRACK_PWD: Optional[str] = None
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+config = Config()
