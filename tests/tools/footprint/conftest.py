5c3f9896730bc55a2e953561b2c10d8861247cca
Craig Pellegrino <craigpellegrino@gmail.com>
2025-04-23 12:13:59 -0400

    fix: linting

--- tests/tools/footprint/conftest.py
+++ tests/tools/footprint/conftest.py
@@ -3,8 +3,8 @@ from typing import Any
 import pytest
 
 from across.tools import Coordinate, Polygon
-from across.tools.footprint import Footprint
 from across.tools.core.schemas import BaseSchema
+from across.tools.footprint import Footprint
 
 
 @pytest.fixture
